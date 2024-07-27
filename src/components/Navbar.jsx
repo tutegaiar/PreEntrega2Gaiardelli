8c2f4070461b545d4a3add5dcec8b36750543376
Matias Gaiardelli <gaiardellimatias@gmail.com>
2024-07-27 13:14:20 -0300

    hacemos el menu responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,29 @@ import { CarWidget } from "./CarWidget"
 import { Logo } from "./Logo"
 import { Menu } from "./Menu"
 import cruz from "../assets/cruz.png"
-export const Navbar = () => {
-const [cliked, setCliked] = useState(false)
-const handleClick = () => {
-    setCliked(!cliked)
-}
 
+export const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    };
+
+    const handleClose = () => {
+        setClicked(false);
+    };
     return (
         <>
         <div className="navbar__Container">
-        <Menu cliked={cliked} handleClick={handleClick} />
+        <Menu clicked={clicked} handleClick={handleClick} />
         <h1>TIENDA MMARG</h1>
         <Logo />
         <BarraDeBusqueda />
         <CarWidget />
     </div>
-    <div className={`categorias__container ${cliked ? 'active' : ''}`}>
+    <div className={`categorias__container ${clicked ? 'active' : ''}`}>        
         <ul>
-            <li id="cruz"><a href="#"><img src={cruz} alt="" /></a></li>
+            <li id="cruz"><a href="#"><img src={cruz} alt="" onClick={handleClose}/></a></li>
             <li><a href="#">Deportes</a></li>
             <li><a href="#">hombres</a></li>
             <li><a href="#">Mujeres</a></li>
